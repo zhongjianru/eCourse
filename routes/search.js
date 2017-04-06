36a2ecfbee7e3616e9e9c3b41266853de35d965a
Kinyu Chung <459251261@qq.com>
2017-04-07 00:22:39 +0800

    首页搜索功能，搜索结果有误

--- routes/search.js
+++ routes/search.js
@@ -5,12 +5,34 @@
 var express = require('express');
 var router = express.Router();
 
+var UserModel = require('../models/users');
+var CourseModel = require('../models/courses');
+
 router.post('/', function (req, res, next) {
   var type = req.fields.type;
   var words = req.fields.query;
   
   if(type.toString() === 'course') {
-    
+    CourseModel.searchCoursesByTitle(words)
+      .then(function (courses) {
+        res.render('search', {
+          subtitle: '搜索结果',
+          result: courses,
+          type: type
+        });
+      })
+      .catch(next);
+  }
+  else {
+    UserModel.searchUsersByName(words)
+      .then(function (users) {
+        res.render('search', {
+          subtitle: '搜索结果',
+          result: users,
+          type:type
+        });
+      })
+      .catch(next);
   }
 });
 
